0e2df821eeed806ccd9c1a05de703d8762a26684
YaseerManasiya <82041766+YaseerManasiya@users.noreply.github.com>
2021-06-01 17:08:52 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBv_A0eJF0jDmNp5yCzrdWI-90aBjripfo",
+  authDomain: "yn-ca352.firebaseapp.com",
+  projectId: "yn-ca352",
+  storageBucket: "yn-ca352.appspot.com",
+  messagingSenderId: "247377273060",
+  appId: "1:247377273060:web:065ef3d0b9811fea900cf1",
+};
